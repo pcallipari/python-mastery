7c4ba9f03a79b0dceeef71f1970cd91f92b4a000
pcallipari <84109546+pcallipari@users.noreply.github.com>
2023-08-06 20:34:26 +1000

    Completed Ex 1.4

--- pcost.py
+++ pcost.py
@@ -1,7 +1,23 @@
-with open(r"Data/portfolio.dat", "r") as f:
-    total_price = 0
-    for line in f.readlines():
-        stock, quantity, value = line.split()
-        total_price += float(quantity) * float(value)
+# with open(r"Data/portfolio3.dat", "r") as f:
+#     total_price = 0
+#     for line in f.readlines():
+#         stock, quantity, value = line.split()
+#         total_price += float(quantity) * float(value)
 
-print(total_price)
+
+def portfolio_cost(input_file: str) -> float:
+    with open(input_file, "r") as f:
+        total_price = 0
+        for line in f.readlines():
+            try:
+                stock, quantity, value = line.split()
+                total_price += int(quantity) * float(value)
+            except ValueError as e:
+                print(f"Couldn't parse line: '{line}'")
+                print(f"Reason: '{e}''")
+
+    return total_price
+
+
+print(portfolio_cost(r"Data/portfolio3.dat"))
+# print(total_price)
