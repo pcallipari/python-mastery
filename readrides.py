9dfa9248f5e06aa34e4356c99390fff7973548dc
pcallipari <84109546+pcallipari@users.noreply.github.com>
2023-08-07 20:32:58 +1000

    Completed Ex 2.1

---
+++ readrides.py
@@ -0,0 +1,46 @@
+# readrides.py
+
+import csv
+
+# from collections import namedtuple
+
+# Row = namedtuple("Row", ["route", "date", "daytype", "rides"])
+
+
+class Row:
+    __slots__ = ["route", "date", "daytype", "rides"]
+
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+def read_rides_as_tuples(filename):
+    """
+    Read the bus ride data as a list of tuples
+    """
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            # record = (route, date, daytype, rides)
+            record = [route, date, daytype, rides]
+            # record = {"route": route, "date": date, "daytype": daytype, "rides": rides}
+            # record = Row(route, date, daytype, rides)
+            records.append(record)
+    return records
+
+
+if __name__ == "__main__":
+    import tracemalloc
+
+    tracemalloc.start()
+    rows = read_rides_as_tuples("Data/ctabus.csv")
+    print("Memory Use: Current %d, Peak %d" % tracemalloc.get_traced_memory())
