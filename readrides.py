e149154696778b9f45d1ee4f052a271a3317fc07
pcallipari <84109546+pcallipari@users.noreply.github.com>
2023-08-07 21:06:45 +1000

    Completed Ex 2_2

--- readrides.py
+++ readrides.py
@@ -37,6 +37,25 @@ def read_rides_as_tuples(filename):
             records.append(record)
     return records
 
+def read_rides_as_dicts(filename):
+    """
+    Read the bus ride data as a list of tuples
+    """
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            # record = (route, date, daytype, rides)
+            # record = [route, date, daytype, rides]
+            record = {"route": route, "date": date, "daytype": daytype, "rides": rides}
+            # record = Row(route, date, daytype, rides)
+            records.append(record)
+    return records
 
 if __name__ == "__main__":
     import tracemalloc
